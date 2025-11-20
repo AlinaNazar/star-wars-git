85aae17b9230ac0cb8528210ca6581b95d1ae441
AlinaNazar <123127386+AlinaNazar@users.noreply.github.com>
2025-11-20 17:45:48 +0200

    First Commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,16 @@ const Contact = () => {
     return (
         <div>
             Contact
+            akgshqruiehjgr-9cpoisbhjr06vhytipogkujc-pwriovthjpgfv,0[9opw\etyr\]eyt
+            enytrj
+            yjh5wr
+            t
+            wrtjhtr
+            j
+            53j
+            56wju
+            j6
+            e
         </div>
     )
 }
