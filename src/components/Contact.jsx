24fda01fdb05d6325f711cc654c0ddbb78c42a7e
AlinaNazar <123127386+AlinaNazar@users.noreply.github.com>
2025-11-25 11:39:21 +0200

    package.json

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,6 @@ const Contact = () => {
     return (
         <div>
             Contact
-            akgshqruiehjgr-9cpoisbhjr06vhytipogkujc-pwriovthjpgfv,0[9opw\etyr\]eyt
-            enytrj
-            yjh5wr
-            t
-            wrtjhtr
-            j
-            iugouyiyh]]ipo
-            ihioh
-
-            iobo
-            53j
-            56wju
-            j6
-            e
         </div>
     )
 }
