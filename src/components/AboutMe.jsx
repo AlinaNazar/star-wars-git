44be0f1c3b94581e218221a071f7193a245ab8e5
AlinaNazar <123127386+AlinaNazar@users.noreply.github.com>
2025-12-02 10:50:24 +0200

    localStorage to useState

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,61 +3,58 @@ import {baseURL, EXPIDATE} from "../utils/constants.js";
 import heroImg from "../assets/main.jpg";
 
 const AboutMe = () => {
-    const [hero, setHero] = useState(null);
+    const [hero, setHero] = useState(() => JSON.parse(localStorage.getItem("hero")));
     //const [heroImg, setHeroImg] = useState();
     useEffect(() => {
-        const hero = JSON.parse(localStorage.getItem("hero"));
-        if (hero && (Date.now() - hero.expiryDate) < EXPIDATE) {
-            setHero(hero.info);
-        } else {
+        // const hero = JSON.parse(localStorage.getItem("hero"));
+        if (!hero?.payload || (Date.now() - hero.expiryDate) > EXPIDATE) {
             fetch(`${baseURL}/v1/peoples/1`)
                 .then((res) => res.json())
                 .then((data) => {
-                    let card = {
-                        'name': data.name,
-                        'gender': data.gender,
-                        'skin color': data.skin_color,
-                        'hair color': data.hair_color,
-                        'height': data.height,
-                        'eye color': data.eye_color,
-                        'weight': data.mass,
-                        'birth year': data.birth_year,
+                    const info = {
+                        payload: {
+                            'name': data.name,
+                            'gender': data.gender,
+                            'skin color': data.skin_color,
+                            'hair color': data.hair_color,
+                            'height': data.height,
+                            'eye color': data.eye_color,
+                            'weight': data.mass,
+                            'birth year': data.birth_year,
+                        },
+                        expiryDate: Date.now(),
                     };
                     //setHeroImg(data.image);
-                    setHero(card);
-                    card = {
-                        info: card,
-                        expiryDate: Date.now()
-                    }
-                    localStorage.setItem("hero", JSON.stringify(card));
+                    setHero(info);
+                    localStorage.setItem("hero", JSON.stringify(info));
                 })
         }
-    }, []);
+    }, [hero]);
 
-    if (!hero) return <div className='far-galaxy'> Loading...</div>;
+    if (!hero?.payload) return <div className='far-galaxy'> Loading...</div>;
 
 
     return (
 
         <>
-            {(!!hero) &&
+            {hero?.payload &&
                 <div className='container mx-auto max-w-4xl bg-black border-2 border-main rounded-3xl overflow-hidden
   shadow-[0_0_25px_rgba(56,189,248,0.8)]'>
                     <div className='grid grid-cols-3 gap-6 p-6'>
                         <h2 className='col-span-3  text-center mb-4 text-[2rem] tracking-[0.12em]'>
-                            {hero.name}
+                            {hero.payload.name}
                         </h2>
                         <div className='col-span-1'>
-                            <img src={heroImg} alt={hero.name}
+                            <img src={heroImg} alt={hero.payload.name}
                                  className='w-full border-2 border-main rounded-2xl object-cover max-h-[330px]'/>
                         </div>
                         <dl className='col-span-2 grid grid-cols-2 gap-x-4 gap-y-2'>
-                            {Object.entries(hero).map(([key, value]) =>
+                            {Object.entries(hero.payload).map(([key, value]) =>
                                 key === 'name' ? null :
-                                    <React.Fragment key={key}>
+                                    <>
                                         <dt className='col-span-1 text-main uppercase text-sm font-bold'>{key}</dt>
                                         <dd className='col-span-1 text-dataColor  mb-1'>{value}</dd>
-                                    </React.Fragment>
+                                    </>
                             )}
                         </dl>
                     </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,13 @@ import {baseURL, EXPIDATE} from "../utils/constants.js";
 import Button from "./ui/Button.jsx";
 
 const Contact = () => {
-    const [planets, setPlanets] = useState([]);
+    const [planets, setPlanets] = useState(() => {
+        let pl = JSON.parse(localStorage.getItem("planets"));
+        if (pl === null) {
+            pl = {payload: [], expiryDate: 0}
+        }
+        return pl;
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -16,44 +22,42 @@ const Contact = () => {
     }
 
     useEffect(() => {
-        const planets = JSON.parse(localStorage.getItem("planets"));
-        if (planets && (Date.now() - planets.expiryDate) < EXPIDATE) {
-            setPlanets(planets.info);
-        } else {
+        // const planets = JSON.parse(localStorage.getItem("planets"));
+        if (planets.payload.length === 0 || (Date.now() - planets.expiryDate) > EXPIDATE) {
             fetch(`${baseURL}/v1/planets`)
-            .then((res) => res.json())
-            .then((data) => {
-                let planetsNames = data.map(p => p.name);
-                setPlanets(planetsNames);
-                planetsNames = {
-                    info: planetsNames,
-                    expiryDate: Date.now(),
-                }
-                localStorage.setItem("planets", JSON.stringify(planetsNames));
-            })
+                .then((res) => res.json())
+                .then((data) => {
+                    const info = {
+                        payload: data.map(p => p.name),
+                        expiryDate: Date.now(),
+                    }
+                    setPlanets(info);
+                    localStorage.setItem("planets", JSON.stringify(info));
+                })
         }
-    }, []);
+    }, [planets]);
 
     return (
         <div className="max-w-[600px] mx-auto my-8 p-8 rounded-3xl bg-black border-main border-2
         shadow-[0_0_20px_rgba(0,0,0,0.9),_0_0_25px_rgba(255,255,0,0.18)] text-gray-100 box-border">
             <form onSubmit={handleSubmit}>
 
-                <label className={'form-label'} htmlFor="fname">First Name</label>
-                <input className={'form-field'} type="text" id="fname" name="firstname" placeholder="Your name.."/>
+                <label className={'form-label'} htmlFor="fName">First Name</label>
+                <input className={'form-field'} type="text" id="fName" name="fName" placeholder="Your name.."/>
 
-                <label className={'form-label'} htmlFor="lname">Last Name</label>
-                <input className={'form-field'} type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+                <label className={'form-label'} htmlFor="lName">Last Name</label>
+                <input className={'form-field'} type="text" id="lName" name="lName" placeholder="Your last name.."/>
 
                 <label className={'form-label'} htmlFor="planet">Planet</label>
                 <select className={'form-field'} id="planet" name="planet">
-                    {planets.map(name => (
+                    {planets.payload.map(name => (
                         <option key={name} value={name}>{name}</option>
                     ))}
                 </select>
 
-                <label  className={'form-label'} htmlFor="subject">Subject</label>
-                <textarea className={'form-field'} id="subject" name="subject" placeholder="Write something.." style={{height: "200px"}}></textarea>
+                <label className={'form-label'} htmlFor="subject">Subject</label>
+                <textarea className={'form-field'} id="subject" name="subject" placeholder="Write something.."
+                          style={{height: "200px"}}></textarea>
 
                 <Button className={'mt-2 text-center text-main'} type="submit" onClick={handleSubmit}>Submit</Button>
 
